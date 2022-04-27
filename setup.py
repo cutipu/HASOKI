14c608b27536e31b2beb125060471a11beccb828
Hisoka <5609903+cutipu@users.noreply.github.com>
2022-04-27 10:22:37 +0700

    add httpx install
    add httpx install

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ if c == "0":
     os.system("pip install socks")
     os.system("pip install pysocks")
     os.system("pip install colorama")
+    os.system("pip install httpx")
     os.system("pip install undetected_chromedriver")
 elif c == "1":
     os.system("pip3 install cloudscraper")
@@ -13,6 +14,7 @@ elif c == "1":
     os.system("pip3 install pysocks")
     os.system("pip3 install colorama")
     os.system("pip3 install undetected_chromedriver")
+    os.system("pip3 install httpx")
 
 if os.name == "nt":
     pass
@@ -20,4 +22,4 @@ else:
     os.system("wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb")
     os.system("apt-get install ./google-chrome-stable_current_amd64.deb")
 
-print("Setup Done.")
+print("Done.")
